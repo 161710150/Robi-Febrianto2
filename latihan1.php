32b19730888800830afa71798bb5727219f91ac8
Robi Febrianto <161710150@smkassalaambandung.sch.id>
2017-10-24 08:47:59 +0700

    project 2

---
+++ latihan1.php
@@ -0,0 +1,78 @@
+<?php
+require_once 'latihan.php';
+
+$latihan1 = new latihan('Robi',11,'bandung','naon weh','Bandung');
+echo  "no :1<br>";
+echo "Namanya...".$latihan1->get_nama();
+echo "<br>";
+echo "kelas...".$latihan1->get_kelas();
+echo "<br>";
+echo "Tempat Lahir...".$latihan1->get_tempatlahir();
+echo "<br>";
+echo "Status...".$latihan1->get_status();
+echo "<br>";
+echo "Alamat...".$latihan1->get_alamat();
+echo "<br>";
+echo "============================================================";
+echo "<br>";
+
+$latihan1 = new latihan('Rafa',12,'bandung','jomblo','Bandung');
+echo  "no :2<br>";
+echo "Namanya...".$latihan1->get_nama();
+echo "<br>";
+echo "kelas...".$latihan1->get_kelas();
+echo "<br>";
+echo "Tempat Lahir...".$latihan1->get_tempatlahir();
+echo "<br>";
+echo "Status...".$latihan1->get_status();
+echo "<br>";
+echo "Alamat...".$latihan1->get_alamat();
+echo "<br>";
+echo "============================================================";
+echo "<br>";
+
+$latihan1 = new latihan('gani',10,'bandung','kawin','Bandung');
+echo  "no :3<br>";
+echo "Namanya...".$latihan1->get_nama();
+echo "<br>";
+echo "kelas...".$latihan1->get_kelas();
+echo "<br>";
+echo "Tempat Lahir...".$latihan1->get_tempatlahir();
+echo "<br>";
+echo "Status...".$latihan1->get_status();
+echo "<br>";
+echo "Alamat...".$latihan1->get_alamat();
+echo "<br>";
+echo "============================================================";
+echo "<br>";
+
+$latihan1 = new latihan('Firas',11,'bandung','nikah','Bandung');
+echo  "no :4<br>";
+echo "Namanya...".$latihan1->get_nama();
+echo "<br>";
+echo "kelas...".$latihan1->get_kelas();
+echo "<br>";
+echo "Tempat Lahir...".$latihan1->get_tempatlahir();
+echo "<br>";
+echo "Status...".$latihan1->get_status();
+echo "<br>";
+echo "Alamat...".$latihan1->get_alamat();
+echo "<br>";
+echo "============================================================";
+echo "<br>";
+
+$latihan1 = new latihan('Alif',11,'bandung','mahasiswa','Bandung');
+echo  "no :5<br>";
+echo "Namanya...".$latihan1->get_nama();
+echo "<br>";
+echo "kelas...".$latihan1->get_kelas();
+echo "<br>";
+echo "Tempat Lahir...".$latihan1->get_tempatlahir();
+echo "<br>";
+echo "Status...".$latihan1->get_status();
+echo "<br>";
+echo "Alamat...".$latihan1->get_alamat();
+echo "<br>";
+echo "============================================================";
+echo "<br>";
+?>
